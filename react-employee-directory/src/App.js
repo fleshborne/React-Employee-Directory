50ad96d1115d38eecc9fd6a2c09502c0dbe7f095
Fleshborne <blakeathompson94@gmail.com>
2020-08-09 13:57:13 -0400

    created images

--- react-employee-directory/src/App.js
+++ react-employee-directory/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Table from './components/Table';
+import Container from 'react-bootstrap/Container';
+import data from './data/employeeData.json';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Container className='wrapper'>
+        <Table data={data} />
+      </Container>
     </div>
   );
 }
--- react-employee-directory/src/components/Table.js
+++ react-employee-directory/src/components/Table.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Table } from 'react-bootstrap/Table';
+import Tables from 'react-bootstrap/Table';
 
 class Table extends React.Component {
   render() {
     return (
-      <div className='teamtable mt-5'>
-        <table
-          id='table'
-          className='table table-striped table-hover table-condensed'
-        ></table>
-      </div>
+      <Tables striped bordered hover variant='dark'>
+        <thead></thead>
+      </Tables>
     );
   }
 }
+
+export default Table;
